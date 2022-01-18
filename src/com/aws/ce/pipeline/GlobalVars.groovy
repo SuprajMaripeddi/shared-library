e87c008bb3b59bf04dc9c577ad8893f616523c30
Supraj Maripeddi <suprajmaripeddi@C02FN3Z4Q6LR.local>
2022-01-18 08:08:19 +0530

    adding shared

---
+++ src/com/aws/ce/pipeline/GlobalVars.groovy
@@ -0,0 +1,7 @@
+package com.aws.ce.pipeline
+ 
+class Common {
+    #testing accessing variables 
+    public static final String GITHUB_PROTOCOL = 'ssh'
+    public static final String GITHUB_HOST = 'github.aws.com'
+}
