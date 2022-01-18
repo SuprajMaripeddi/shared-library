61763bde60f778d9ad55529bc2f82870e8106b9e
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-18 11:05:31 +0530

    Update maven.groovy

--- jenkinsfiles/maven.groovy
+++ jenkinsfiles/maven.groovy
@@ -1,6 +1,6 @@
 @Library('shared-library') _
 
-import com.aws.ce.pipeline.GlobalVars
+//import com.aws.ce.pipeline.GlobalVars
 
 
 jenkinsForJava 'https://github.com/iamvickyav/spring-boot-data-H2-embedded.git'
