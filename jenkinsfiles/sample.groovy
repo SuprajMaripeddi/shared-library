3bd481aaa97381fb73e75e94d32b3872bffae03f
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-18 17:20:29 +0530

    Create sample.groovy

---
+++ jenkinsfiles/sample.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+
+   //  Checking Environment Variables
+ 
+    stages {
+        stage ('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                    echo "JAVA_HOME = ${JAVA_HOME}"
+                '''
+            }
+        }
+
+        stage ('git clone') {
+            steps {
+              git branch: 'master', url: "https://github.com/SuprajMaripeddi/shared-library.git" 
+            }
+        }
+        stage ('Build') {
+            steps {
+                sh "mvn clean install -DskipTests"
+            }
+        }      
+    }
+}
