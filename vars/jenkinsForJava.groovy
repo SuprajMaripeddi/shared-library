d01b70a3572243ae1d14d04aabeb211bff51f3a7
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-18 11:28:34 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -6,12 +6,11 @@
 // jenkinsForJava.groovy
 def call(String repoUrl) {
  pipeline {
-    agent  any {
-
-            image 'maven:3-alpine'
-            args '-v /root/.m2:/root/.m2'
-
+agent any
+    tools {
+        maven 'apache-maven-3.0.1' 
     }
+
     stages {
         stage('Build') {
             steps {
