82991c208f717513e392c5988086925441cb0aaf
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-18 08:14:30 +0530

    Update GlobalVars.groovy

--- src/com/aws/ce/pipeline/GlobalVars.groovy
+++ src/com/aws/ce/pipeline/GlobalVars.groovy
@@ -1,7 +1,6 @@
 package com.aws.ce.pipeline
- 
+ //Testing variables
 class Common {
-    #testing accessing variables 
     public static final String GITHUB_PROTOCOL = 'ssh'
     public static final String GITHUB_HOST = 'github.aws.com'
 }
