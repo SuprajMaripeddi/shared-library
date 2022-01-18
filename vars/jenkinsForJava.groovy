7440ca0647c9014ec9844fd50c06b5a56a02d093
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-18 11:33:56 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -1,76 +1,43 @@
-
 // vars
 // | --- welcomeJob.groovy
 // | --- jenkinsForJava.groovy
- 
-// jenkinsForJava.groovy
-def call(String repoUrl) {
- pipeline {
-agent any
-    tools {
-        maven 'apache-maven-3.0.1' 
-    }
 
-    stages {
-        stage('Build') {
-            steps {
-                sh 'mvn -B -DskipTests clean package'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh 'mvn test'
-            }
-            post {
-                always {
-                    junit 'target/surefire-reports/*.xml'
-                }
-            }
-        }
-        stage('Deliver') {
-            steps {
-                sh './jenkins/scripts/deliver.sh'
-            }
-        }
-    }
-}
-//   pipeline {
-//        agent any
-//        tools {
-//           maven 'Maven 3.3.9'
-//             jdk "jdk-1.8.101"
-//        }
-//   steps {
-//                     sh 'java -version'
-//                 }
-//        stages {
-//            stage("Tools initialization") {
-//                steps {
-//                    sh "mvn --version"
-//                    sh "java -version"
-//                }
-//            }
-//            stage("Checkout Code") {
-//                steps {
-//                    git branch: 'master',
-//                        url: "${repoUrl}"
-//                }
-//            }
-//            stage("Cleaning workspace") {
-//                steps {
-//                    sh "mvn clean"
-//                }
-//            }
-//            stage("Running Testcase") {
-//               steps {
-//                    sh "mvn test"
-//                }
-//            }
-//            stage("Packing Application") {
-//                steps {
-//                    sh "mvn package -DskipTests"
-//                }
-//            }
-//        }
-//    }
+// // jenkinsForJava.groovy
+def call(String repoUrl) {
+  pipeline {
+       agent any
+       tools { 
+           maven 'apache-maven-3.0.1' 
+           jdk 'jdk8'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
 }
