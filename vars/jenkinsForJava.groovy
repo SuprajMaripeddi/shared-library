7fac5d0d412c19519fc37a21b37f720733dfdb7d
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-18 11:16:08 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -5,43 +5,73 @@
  
 // jenkinsForJava.groovy
 def call(String repoUrl) {
-  pipeline {
-       agent any
-       tools {
-          maven 'Maven 3.3.9'
-            jdk "jdk-1.8.101"
-       }
-  steps {
-                    sh 'java -version'
+ pipeline {
+    agent {
+        docker {
+            image 'maven:3-alpine'
+            args '-v /root/.m2:/root/.m2'
+        }
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
                 }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'master',
-                       url: "${repoUrl}"
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   sh "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
-       }
-   }
+            }
+        }
+        stage('Deliver') {
+            steps {
+                sh './jenkins/scripts/deliver.sh'
+            }
+        }
+    }
+}
+//   pipeline {
+//        agent any
+//        tools {
+//           maven 'Maven 3.3.9'
+//             jdk "jdk-1.8.101"
+//        }
+//   steps {
+//                     sh 'java -version'
+//                 }
+//        stages {
+//            stage("Tools initialization") {
+//                steps {
+//                    sh "mvn --version"
+//                    sh "java -version"
+//                }
+//            }
+//            stage("Checkout Code") {
+//                steps {
+//                    git branch: 'master',
+//                        url: "${repoUrl}"
+//                }
+//            }
+//            stage("Cleaning workspace") {
+//                steps {
+//                    sh "mvn clean"
+//                }
+//            }
+//            stage("Running Testcase") {
+//               steps {
+//                    sh "mvn test"
+//                }
+//            }
+//            stage("Packing Application") {
+//                steps {
+//                    sh "mvn package -DskipTests"
+//                }
+//            }
+//        }
+//    }
 }
