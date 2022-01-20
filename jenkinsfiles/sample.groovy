68ed0fbf1df7d86331349d4c34f7d5c7828e0652
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-20 09:57:05 +0530

    Update sample.groovy

--- jenkinsfiles/sample.groovy
+++ jenkinsfiles/sample.groovy
@@ -31,7 +31,8 @@ pipeline {
         stage ('Build') {
             steps {
                 sh "mvn clean install package -DskipTests"
-                nexusArtifactUploader credentialsId: 'nexus', groupId: 'com.mycompany.app', nexusUrl: 'http://52.15.201.38:8081/', nexusVersion: 'nexus2', protocol: 'http', repository: 'scm', version: '1.0-SNAPSHOT'
+                nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
+                //nexusArtifactUploader credentialsId: 'nexus', groupId: 'com.mycompany.app', nexusUrl: 'http://52.15.201.38:8081/', nexusVersion: 'nexus2', protocol: 'http', repository: 'scm', version: '1.0-SNAPSHOT'
             }
         }      
     }
