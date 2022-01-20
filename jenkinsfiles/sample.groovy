566892d2008a3e763208aeb55b8a1de1c6cea2dc
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-20 11:03:57 +0530

    Update sample.groovy

--- jenkinsfiles/sample.groovy
+++ jenkinsfiles/sample.groovy
@@ -15,36 +15,36 @@ pipeline {
                 '''
             }
         }
-    }
 
-    stage ('git clone') {
+        stage ('git clone') {
+                steps {
+                git branch: 'master', url: "https://github.com/jenkins-docs/simple-java-maven-app.git"
+                }
+        }
+        stage ('Scan and Build Jar File') {
             steps {
-              git branch: 'master', url: "https://github.com/jenkins-docs/simple-java-maven-app.git"
-            }
-    }
-    stage ('Scan and Build Jar File') {
-        steps {
-            withSonarQubeEnv(installationName: 'SonarQube', credentialsId: 'sonar') {
-                sh 'mvn sonar:sonar -Dsonar.analysis.mode='
+                withSonarQubeEnv(installationName: 'SonarQube', credentialsId: 'sonar') {
+                    sh 'mvn sonar:sonar -Dsonar.analysis.mode='
+                }
             }
         }
-    }
-    stage ('Build') {
-        steps {
-            sh "mvn clean install package -DskipTests"
-                //nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
-        } 
-    }
-    stage ('Publish') {
-        steps {
-                //nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
-            nexusArtifactUploader artifacts: [[artifactId: 'my-app', classifier: '', file: '/var/lib/jenkins/workspace/devopp/target/my-app-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus', groupId: 'com.mycompany.app', nexusUrl: '52.15.201.38:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'scm', version: '1.0-SNAPSHOT'            
-        } 
-    }
-    stage ('deploy') {
-        steps {
-            deploy adapters: [tomcat9(credentialsId: 'tomcat', path: '', url: 'http://3.138.245.150:8080')], contextPath: null, war: '**/*.jar'
-                //nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
-        }       
+        stage ('Build') {
+            steps {
+                sh "mvn clean install package -DskipTests"
+                    //nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
+            } 
+        }
+        stage ('Publish') {
+            steps {
+                    //nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
+                nexusArtifactUploader artifacts: [[artifactId: 'my-app', classifier: '', file: '/var/lib/jenkins/workspace/devopp/target/my-app-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus', groupId: 'com.mycompany.app', nexusUrl: '52.15.201.38:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'scm', version: '1.0-SNAPSHOT'            
+            } 
+        }
+        stage ('deploy') {
+            steps {
+                deploy adapters: [tomcat9(credentialsId: 'tomcat', path: '', url: 'http://3.138.245.150:8080')], contextPath: null, war: '**/*.jar'
+                    //nexusPublisher nexusInstanceId: 'releases', nexusRepositoryId: 'scm', packages: [[$class: 'MavenPackage', mavenAssetList: [], mavenCoordinate: [artifactId: 'my-app', groupId: 'com.mycompany.app', packaging: 'jar', version: '1.0-SNAPSHOT']]]
+            }       
+        }
     }
 }
