759029464746f6a1657e4c436c4cf365f9ebd5bf
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-21 11:21:55 +0530

    Update withSonar.groovy

--- vars/withSonar.groovy
+++ vars/withSonar.groovy
@@ -1,4 +1,5 @@
 void call(Map opts) {
+  echo "${opts.installationName}"
   withSonarQubeEnv(installationName: '${opts.installationName}', credentialsId: '${opts.credentialsId}') {
     sh 'mvn sonar:sonar -Dsonar.analysis.mode='
   }
