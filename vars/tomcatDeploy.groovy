f1782e44d29343cb3e803f27715cabae2016d63c
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-21 11:37:58 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,3 +1,3 @@
 void call(Map opts) {            
-  deploy adapters: [tomcat9(credentialsId: '${opts.credentialsId}', path: '${opts.path}', url: '${opts.url}')], contextPath: '${opts.contextPath}', war: '${opts.war}'
+  deploy adapters: [tomcat9(credentialsId: "${opts.credentialsId}", path: "${opts.path}", url: "${opts.url}")], contextPath: "${opts.contextPath}", war: "${opts.war}"
 }
