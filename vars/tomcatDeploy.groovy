0d212b353d39b5207e6b620a86e193768473b2fa
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-21 10:44:19 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,3 @@
+void call(Map opts) {            
+  deploy adapters: [tomcat9(credentialsId: '${opts.credentialsId}', path: '${opts.path}', url: '${opts.url}')], contextPath: '${opts.contextPath}', war: '${opts.war}'
+}
