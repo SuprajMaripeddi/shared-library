a7e0bfb0fbb51dc3d6e103d3f9166ba945b191e6
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-21 10:41:41 +0530

    Create nexusArtifcatUploader.groovy

---
+++ vars/nexusArtifcatUploader.groovy
@@ -0,0 +1,4 @@
+
+void call(Map opts) {
+  nexusArtifactUploader artifacts: [[artifactId: '${opts.artifactId}', classifier: '${opts.classifier}', file: '${opts.file}', type: '${opts.type}']], credentialsId: '${opts.credentialsId}', groupId: '${opts.groupId}', nexusUrl: '${opts.nexusUrl}', nexusVersion: '${opts.nexusVersion}', protocol: '${opts.protocol}', repository: '${opts.repository}', version: '${opts.version}'
+}
