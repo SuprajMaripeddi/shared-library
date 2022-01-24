c9b58bc532f9fd191cb114ab78d17cb372bdfa36
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-01-24 10:00:22 +0530

    Update build.groovy

--- jenkinsfiles/build.groovy
+++ jenkinsfiles/build.groovy
@@ -40,7 +40,7 @@ pipeline {
                     nexusVersion: 'nexus3', 
                     protocol: 'http', 
                     repository: 'scm', 
-                    version: '2.0' 
+                    version: '2.0-SNAPSHOT' 
                 )           
             } 
         }
