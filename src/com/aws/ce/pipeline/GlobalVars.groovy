9900e8c4215cb399959188505fdb8bf8432d750a
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-03-09 14:13:21 +0530

    Update GlobalVars.groovy

--- src/com/aws/ce/pipeline/GlobalVars.groovy
+++ src/com/aws/ce/pipeline/GlobalVars.groovy
@@ -1,5 +1,6 @@
 package com.aws.ce.pipeline
- //Testing variables
 class Common {
     public static final String WarFilePath = '/var/lib/jenkins/workspace/devopp/target/'
+    public static final String MAVEN = "Apache Maven 3.3.9";
+    public static final String GITHUB_CREDENTIALS_ID = "place your github credentials id ";
 }
